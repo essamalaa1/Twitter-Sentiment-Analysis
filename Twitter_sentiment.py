d3883595970d0a1a835041d955fe9eeb8a22e8bd
Essam Alaa <157516664+essamalaa1@users.noreply.github.com>
2025-03-24 00:24:26 +0200

    Twitter_sentiment.py

--- Twitter_sentiment.py
+++ Twitter_sentiment.py
@@ -1,4 +1,3 @@
-# %%
 import pandas as pd
 import numpy as np
 import time
@@ -23,22 +22,10 @@ import psutil
 
 df = pd.read_csv(r"DATA.csv", encoding='ISO-8859-1')
 df = df.drop("1467810369",axis=1)
-
-# %%
 df.columns = ['polarity', 'date', 'query', 'user', 'text']
 
-df.head()
-
-# %%
 df = df.drop(["query","date","user"],axis=1)
 
-# %%
-df.head()
-
-# %%
-df.tail()
-
-# %%
 print("Shape of the DataFrame:", df.shape)
 print("\nPolarity Value Counts:")
 print(df["polarity"].value_counts())
@@ -47,11 +34,7 @@ print(df["polarity"].unique())
 print("\nMissing (null) Values in the DataFrame:")
 print(df.isnull().sum())
 
-
-# %%
 df["polarity"] = df["polarity"].replace(4, 1)
-
-# %%
 print("Shape of the DataFrame:", df.shape)
 print("\nPolarity Value Counts:")
 print(df["polarity"].value_counts())
@@ -60,8 +43,6 @@ print(df["polarity"].unique())
 print("\nMissing (null) Values in the DataFrame:")
 print(df.isnull().sum())
 
-
-# %%
 X = df['text']
 y = df['polarity']
 
@@ -72,8 +53,6 @@ X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, r
 train_df = pd.DataFrame({'text': X_train, 'polarity': y_train})
 val_df = pd.DataFrame({'text': X_val, 'polarity': y_val})
 test_df = pd.DataFrame({'text': X_test, 'polarity': y_test})
-
-# %%
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
@@ -93,26 +72,21 @@ def preprocess_tweet(tweet):
 
     return extra_whitespace_pattern.sub(' ', ' '.join(tokens)).strip()
 
-
-# %% [markdown]
 # Remove URLs
-# 
+
 # Remove Mentions
-# 
+
 # Remove Hashtags
-# 
+
 # Remove Special Characters and Lowercasing     
-# 
+
 # Remove Stopwords , Tokenization , Lemmatization
-# 
+
 # Remove Extra Whitespaces
 
-# %%
 train_df = train_df.sample(frac=1, random_state=42).reset_index(drop=True)
 val_df = val_df.sample(frac=1, random_state=42).reset_index(drop=True)
 test_df = test_df.sample(frac=1, random_state=42).reset_index(drop=True)
-
-# %%
 sample_size = 0.5
 
 train_df = train_df.groupby(train_df.iloc[:, 1]).apply(lambda x: x.sample(frac=sample_size, random_state=42)).reset_index(drop=True)
@@ -122,26 +96,18 @@ val_df = val_df.groupby(val_df.iloc[:, 1]).apply(lambda x: x.sample(frac=sample_
 print(train_df.iloc[:, 1].value_counts(normalize=True))
 print(test_df.iloc[:, 1].value_counts(normalize=True))
 print(val_df.iloc[:, 1].value_counts(normalize=True))
-
-# %%
 train_df['text'] = train_df['text'].apply(preprocess_tweet)
 val_df['text'] = val_df['text'].apply(preprocess_tweet)
 test_df['text'] = test_df['text'].apply(preprocess_tweet)
-
-# %%
 train_texts, train_labels = train_df["text"], train_df["polarity"]
 test_texts, test_labels = test_df["text"], test_df["polarity"]
 val_texts, val_labels = val_df["text"], val_df["polarity"]
-
-# %%
 train_texts = train_texts.astype(str)
 test_texts = test_texts.astype(str)
 val_texts = val_texts.astype(str)
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(train_texts)
-
-# %%
 total_words = sum(tokenizer.word_counts.values())
 sorted_counts = sorted(tokenizer.word_counts.items(), key=lambda x: x[1], reverse=True)
 
@@ -157,42 +123,32 @@ for word, count in sorted_counts:
 
 print(f"num_words to cover 95% of the word occurrences: {num_words}")
 
-# %% [markdown]
-# The cell above help us identify number of words in the below cell
+# The line above help us identify number of words in the below cell
 
-# %%
 tokenizer = Tokenizer(num_words=num_words)
 tokenizer.fit_on_texts(train_texts)
 
 vocab_size = len(tokenizer.word_index) + 1
 print(f"Final vocabulary size: {vocab_size}")
-
-# %%
 train_sequences = tokenizer.texts_to_sequences(train_texts)
 test_sequences = tokenizer.texts_to_sequences(test_texts)
 val_sequences = tokenizer.texts_to_sequences(val_texts)
 
-# %% [markdown]
 # Convert Text to sequence of Numeric Format
 
-# %%
 sequence_lengths = [len(seq) for seq in train_sequences]
 
 print(f"Max length: {np.max(sequence_lengths)}")
 
-# %% [markdown]
 # The cell above help us identify sequence_lengths in the below cell
 
-# %%
 max_sequence_length = np.max(sequence_lengths)
 train_padded = pad_sequences(train_sequences, maxlen=max_sequence_length)
 test_padded = pad_sequences(test_sequences, maxlen=max_sequence_length)
 val_padded = pad_sequences(val_sequences, maxlen=max_sequence_length)
 
-# %% [markdown]
 # Make all input data of the same shape by padding or truncating
 
-# %%
 trainable_model = Sequential([
     Embedding(input_dim=vocab_size, output_dim=50, input_length=max_sequence_length, trainable=True),
     LSTM(128, dropout=0.4, recurrent_dropout=0),
@@ -201,8 +157,6 @@ trainable_model = Sequential([
 
 trainable_model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 trainable_model.summary()
-
-# %%
 checkpoint_dir = "model_checkpoints"
 os.makedirs(checkpoint_dir, exist_ok=True)
 
@@ -218,14 +172,10 @@ model_checkpoint = ModelCheckpoint(
     save_weights_only=True,
     verbose=1
 )
-
-# %%
 trainable_model.fit(train_padded, train_labels, 
                     epochs=50, batch_size=64, 
                     validation_data=(val_padded, val_labels),
                     callbacks=[early_stop, model_checkpoint])
-
-# %%
 # LSTM Layer Parameters
 lstm_weights = trainable_model.layers[1].get_weights()
 W_i, U_i, b_i = lstm_weights[0][:, :128], lstm_weights[1][:, :128], lstm_weights[2][:128]  # Input gate
@@ -235,11 +185,6 @@ W_o, U_o, b_o = lstm_weights[0][:, 384:], lstm_weights[1][:, 384:], lstm_weights
 
 # Dense Layer Parameters
 dense_weights, dense_bias = trainable_model.layers[2].get_weights()
-
-# %% [markdown]
-# Extract Model Weights for the forward pass
-
-# %%
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -282,8 +227,6 @@ def predict(batch):
 
     return np.array(predictions)
 
-
-# %%
 def get_memory_usage():
     process = psutil.Process()
     memory_info = process.memory_info()
@@ -319,60 +262,42 @@ print(f"Custom Function Time: {custom_time:.4f} seconds")
 print(f"Model Function Time: {model_time:.4f} seconds")
 print(f"Custom Function Memory: {custom_memory:.4f} MB")
 print(f"Model Function Memory: {model_memory:.4f} MB")
-
-# %% [markdown]
 # The custom implementation achieves high accuracy, nearly identical to the trained model.The minor differences (accuracy less than 100%) may arise due to:
-# 
-# Numerical precision differences between NumPy operations and TensorFlow optimizations.
-# 
-# The manual sigmoid and tanh functions might have slight deviations compared to their TensorFlow counterparts.
 
-# %% [markdown]
+# Numerical precision differences between NumPy operations and TensorFlow optimizations.
+
+# The manual sigmoid and tanh functions might have slight deviations compared to their TensorFlow counterparts.
 # 1) TensorFlow Optimization:
-# 
+
 # TensorFlow is highly optimized for GPU and parallelized computation, enabling rapid forward passes for batches of data. It leverages cuDNN, to process LSTM computations efficiently.
-# 
+
 # 2) Custom Function:
-# 
+
 # The custom function processes the input sequentially for each timestep and sequence, leading to significant overhead.
 # It runs on the CPU using NumPy, which is not optimized for deep learning computations.
 # Masking is implemented manually, adding additional computation for each timestep.
-
-# %% [markdown]
 # 1) TensorFlow's Overheads:
-# 
+
 # TensorFlow allocates additional memory for internal graph representations, tensors, and other intermediate computations to optimize performance and enable gradient computations.
-# 
+
 # 2) Custom Function :
 # The custom function only computes the forward pass without the overhead of maintaining computational graphs or intermediate states.
 # It uses NumPy arrays, which have lower memory overhead compared to TensorFlow's tensor objects.
-
-# %%
 trainable_acc = trainable_model.evaluate(test_padded, test_labels, batch_size=64)
 print(f"Trainable Embeddings Model Accuracy: {trainable_acc[1]:.4f}")
-
-# %%
 y_pred_probs = trainable_model.predict(test_padded, batch_size=64)
 y_pred = (y_pred_probs > 0.5).astype("int32")
-
-# %%
 conf_mat = confusion_matrix(test_labels, y_pred)
 
 disp = ConfusionMatrixDisplay(confusion_matrix=conf_mat, display_labels=["Class 0", "Class 1"])
 disp.plot(cmap=plt.cm.Blues)
 plt.title("Confusion Matrix - Trainable Embeddings Model")
 plt.show()
-
-# %%
 report = classification_report(test_labels, y_pred, target_names=["Class 0", "Class 1"])
 print("Classification Report:\n")
 print(report)
-
-# %%
 model_path = r"C:\Users\Essam\Desktop\DL Assignment2\Word2vec\word2vec-google-news-300\word2vec-google-news-300.gz"
 word2vec_model = KeyedVectors.load_word2vec_format(model_path, binary=True)
-
-# %%
 unmatched_count = 0
 matched_count = 0
 
@@ -385,11 +310,7 @@ for word in tokenizer.word_index.keys():
 print(f"Matched tokens: {matched_count}")
 print(f"Unmatched tokens (OOV): {unmatched_count}")
 
-
-# %% [markdown]
 # number of words not covered by word2vec embedding
-
-# %%
 def build_embedding_matrix(word_index, embedding_dim=300):
     embedding_matrix = np.zeros((len(word_index) + 1, embedding_dim))
     for word, i in word_index.items():
@@ -409,8 +330,6 @@ pretrained_model = Sequential([
 
 pretrained_model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 pretrained_model.summary()
-
-# %%
 checkpoint_dir = "model_checkpoints"
 os.makedirs(checkpoint_dir, exist_ok=True)
 
@@ -426,14 +345,10 @@ model_checkpoint_pretrained  = ModelCheckpoint(
     save_weights_only=True,
     verbose=1
 )
-
-# %%
 pretrained_model.fit(train_padded, train_labels,
                     epochs=50, batch_size=64,
                     validation_data=(val_padded, val_labels),
                     callbacks=[early_stop_pretrained, model_checkpoint_pretrained])
-
-# %%
 # Extract LSTM Layer Parameters from the pretrained model
 lstm_weights = pretrained_model.layers[1].get_weights()
 W_i, U_i, b_i = lstm_weights[0][:, :128], lstm_weights[1][:, :128], lstm_weights[2][:128]  # Input gate
@@ -443,8 +358,6 @@ W_o, U_o, b_o = lstm_weights[0][:, 384:], lstm_weights[1][:, 384:], lstm_weights
 
 # Extract Dense Layer Parameters from the pretrained model
 dense_weights, dense_bias = pretrained_model.layers[2].get_weights()
-
-# %%
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -486,8 +399,6 @@ def predict(batch):
 
     return np.array(predictions)
 
-
-# %%
 def get_memory_usage():
     process = psutil.Process()
     memory_info = process.memory_info()
@@ -524,28 +435,16 @@ print(f"Model Function Time: {model_time:.4f} seconds")
 print(f"Custom Function Memory: {custom_memory:.4f} MB")
 print(f"Model Function Memory: {model_memory:.4f} MB")
 
-
-# %%
 test_loss, test_acc = pretrained_model.evaluate(test_padded, test_labels)
 print(f"Test Accuracy: {test_acc}")
-
-# %%
 test_preds = pretrained_model.predict(test_padded)
 test_preds = (test_preds > 0.5).astype(int)
-
-# %%
 conf_mat = confusion_matrix(test_labels, test_preds)
 
 disp = ConfusionMatrixDisplay(confusion_matrix=conf_mat, display_labels=["Class 0", "Class 1"])
 disp.plot(cmap=plt.cm.Blues)
 plt.title("Confusion Matrix - Trainable Embeddings Model")
 plt.show()
-
-# %%
 report = classification_report(test_labels, test_preds, target_names=["Negative", "Positive"])
 print(report)
-
-# %% [markdown]
-# ### Both Models Trained and Pre-trained achieving simillar accuracies but the pretrained model is much faster as it is using pretrained embeddings 
-
-
+### Both Models Trained and Pre-trained achieving simillar accuracies but the pretrained model is much faster as it is using pretrained embeddings 
